c334f62a4e32df55c6eec3eaaea49c22cd1de94b
Daniel Chuks Okocha <Dani.okocha@gmail.com>
2015-09-09 12:02:29 +0100

    Github first upload

---
+++ jeffs_array.rb
@@ -0,0 +1,11 @@
+class JeffsArray
+  def initialize(jeffs_array)
+    @jeffs_array = jeffs_array
+  end
+  def new_pop
+    @jeffs_array.pop
+  end
+  def new_size
+    @jeffs_array.size
+  end
+end
